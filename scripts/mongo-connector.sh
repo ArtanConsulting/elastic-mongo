f3e6059b6fc55159f06d52a6598e2dbb87264691
harshadyeola <harshadyeola92@gmail.com>
2016-10-27 15:31:47 +0530

    working elastic-mongo setup

--- scripts/mongo-connector.sh
+++ scripts/mongo-connector.sh
@@ -6,6 +6,7 @@ ES=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
 pip install mongo-connector
 pip install elastic-doc-manager
+apt-get update && apt-get install -y curl
 touch /scripts/mongo-connector-installed
 
 printf "\n\nWaiting for MongoDB to start\n"
@@ -16,4 +17,4 @@ done
 echo "MongoDB has started!"
 
 echo "\n\nStarting mongo-connector.."
-mongo-connector --auto-commit-interval=5 -m ${MONGO}:27017 -t ${ES} -v -d elastic_doc_manager --stdout
+mongo-connector --auto-commit-interval=5 -m mongo1:27017 -t elasticsearch:9200 -v -d elastic_doc_manager --stdout
